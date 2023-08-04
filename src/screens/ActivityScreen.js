f7ceef6191c1970e4426b78c902c21b5e738050f
Yearsky <43109148+yearsky@users.noreply.github.com>
2023-08-04 21:20:57 +0700

    initiate flatlist

--- src/screens/ActivityScreen.js
+++ src/screens/ActivityScreen.js
@@ -8,7 +8,11 @@ import {
   TouchableOpacity,
   View,
   Button,
+  RefreshControl,
+  Dimensions,
   useWindowDimensions,
+  TouchableWithoutFeedback,
+  TextInput,
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "../../assets/styles/GlobalStyles";
@@ -22,7 +26,7 @@ import {
   BottomSheetBackdrop,
 } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import CustomBackdrop from "../components/CustomBackDrop";
+import { activitySlice } from "../store/activitySlice";
 
 const ActivityScreen = ({ navigation }) => {
   // const navigation = useNavigation();
@@ -30,7 +34,12 @@ const ActivityScreen = ({ navigation }) => {
   const [selectedTab, setSelectedTab] = useState("");
   const [isVisible, setVisible] = useState(false);
   const { width } = useWindowDimensions();
+  const [refreshing, setRefreshing] = useState(false);
   const dataActivity = useSelector((state) => state.activityItems);
+
+  const selectedActivity = useSelector(
+    (state) => state.activityItems.selectedActivity
+  );
   const tabs = useSelector((state) => state.tab.tab);
 
   const bottomSheetModalRef = useRef(null);
@@ -39,92 +48,113 @@ const ActivityScreen = ({ navigation }) => {
   const snapPoints = useMemo(() => ["15%", "50%"], []);
 
   // callbacks
-  const handlePresentModalPress = useCallback(() => {
-    bottomSheetModalRef.current?.present();
+  const handlePresentModalPress = useCallback(async (item) => {
+    dispatch(activitySlice.actions.setSelectedActivity(item.id));
+    await bottomSheetModalRef.current?.present({ selectedActivity });
   }, []);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
+  const renderItem = useCallback(
+    ({ item }) => (
+      <TouchableOpacity
+        key={item.id}
+        className="flex-row bg-white rounded-xl items-center my-2 p-3"
+        onPress={() => handlePresentModalPress(item)}
+      >
+        <Image
+          source={require("../../assets/login.png")}
+          style={{ width: 50, height: 50 }}
+          className="rounded-full bg-white mx-1"
+        />
+        <View className="flex-col">
+          <Text className="text-black font-bold">{item.tiketId}</Text>
+          <Text className="text-slate-400">{item.activity}</Text>
+        </View>
+        <Text className="ml-auto">{item.date}</Text>
+      </TouchableOpacity>
+    ),
+    []
+  );
 
   return (
     <>
       <GestureHandlerRootView>
         <BottomSheetModalProvider>
-          <ScrollView>
-            <View>
-              <View className="pb-0 bg-[#E9B384]">
-                <SafeAreaView>
-                  <View className="m-5 flex-row items-center justify-between">
-                    <Text className="text-white text-2xl font-bold">
-                      Aktivitas Anda
-                    </Text>
-
-                    <TouchableOpacity>
-                      <Ionicons
-                        name="search-circle-outline"
-                        size={40}
-                        color="white"
-                      />
-                    </TouchableOpacity>
-                  </View>
-                </SafeAreaView>
-              </View>
-              <View className="bg-[#A1CCD1] mx-5 mt-10 rounded-xl">
-                <View className="flex-row items-center mx-10 justify-between">
-                  <View className="flex-col">
-                    <Text className="text-white text-lg font-bold">
-                      Total Aktivitas Anda
-                    </Text>
-                    <Text className="text-center text-white font-semibold">
-                      {dataActivity.activityItems.length} Data
-                    </Text>
-                  </View>
-                  <Image
-                    source={require("../../assets/document.png")}
-                    style={{ width: 100, height: 100 }}
-                  />
+          <View>
+            <View className="pb-0 bg-[#E9B384] rounded-b-3xl">
+              <SafeAreaView>
+                <View className="m-5 flex-col items-center justify-center">
+                  <Text className="text-white text-2xl font-bold">
+                    Aktivitas Anda
+                  </Text>
                 </View>
-              </View>
-
-              <View className="mt-10 mx-5">
-                <View className="flex-row justify-center gap-x-2">
-                  <TouchableOpacity className="bg-white h-8 w-20 flex-row justify-center border-blue-500 border-2 items-center rounded-xl">
-                    <Text>Semua</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity className="bg-white h-8 w-20 flex-row justify-center border-black border-2 items-center rounded-xl">
-                    <Text>Hari Ini</Text>
-                  </TouchableOpacity>
-                  {/* <TouchableOpacity className="bg-white h-8 w-20 flex-row justify-center border-black border-2 items-center rounded-xl">
-              <Text> {"<"} 7 Hari </Text>
-            </TouchableOpacity> */}
-                  <TouchableOpacity className="bg-white h-8 w-28 gap-x-2 flex-row justify-center border-black border-2 items-center rounded-xl">
-                    <Text>Filter</Text>
-                    <Ionicons name="filter" size={24} color="black" />
-                  </TouchableOpacity>
+              </SafeAreaView>
+            </View>
+            <View className="bg-[#A1CCD1] mx-5 mt-5 rounded-xl">
+              <View className="flex-row items-center mx-10 justify-between">
+                <View className="flex-col">
+                  <Text className="text-white text-lg font-bold">
+                    Total Aktivitas Anda
+                  </Text>
+                  <Text className="text-center text-white font-semibold">
+                    {dataActivity.activityItems.length} Data
+                  </Text>
                 </View>
-              </View>
-
-              <View className="m-5">
-                {dataActivity.activityItems.map((item) => (
-                  <TouchableOpacity
-                    key={item.id}
-                    className="flex-row bg-white rounded-xl items-center my-2 p-5"
-                    onPress={handlePresentModalPress}
-                  >
-                    <Image
-                      source={require("../../assets/login.png")}
-                      style={{ width: 50, height: 50 }}
-                      className="rounded-full bg-white mx-1"
-                    />
-                    <View className="flex-col">
-                      <Text className="text-black font-bold">
-                        {item.tiketId}
-                      </Text>
-                      <Text className="text-slate-400">{item.activity}</Text>
-                    </View>
-                    <Text className="ml-auto">{item.date}</Text>
-                  </TouchableOpacity>
-                ))}
+                <Image
+                  source={require("../../assets/document.png")}
+                  style={{ width: 100, height: 100 }}
+                />
               </View>
             </View>
-          </ScrollView>
+
+            <View className="mt-10 mx-5">
+              <View className="flex-row justify-center gap-x-2">
+                <TouchableOpacity className="bg-white h-8 w-20 flex-row justify-center border-blue-500 border-2 items-center rounded-xl">
+                  <Text>Semua</Text>
+                </TouchableOpacity>
+                <TouchableOpacity className="bg-white h-8 w-20 flex-row justify-center border-black border-2 items-center rounded-xl">
+                  <Text>Hari Ini</Text>
+                </TouchableOpacity>
+                <TouchableOpacity className="bg-white h-8 w-28 gap-x-2 flex-row justify-center border-black border-2 items-center rounded-xl">
+                  <Text>Filter</Text>
+                  <Ionicons name="filter" size={24} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity className="bg-white h-8 w-8 flex-row justify-center border-black border-2 items-center rounded-xl">
+                  <Ionicons name="search" size={24} color="black" />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View className="mx-5 mt-5">
+              <View
+                style={{
+                  height: Dimensions.get("window").height * 0.5,
+                  paddingBottom: Dimensions.get("window").height * 0.02,
+                }}
+              >
+                <FlatList
+                  data={dataActivity.activityItems}
+                  keyExtractor={(item) => item.id.toString()}
+                  showsVerticalScrollIndicator={false}
+                  renderItem={renderItem}
+                  contentContainerStyle={{
+                    paddingBottom: Dimensions.get("window").height * 0.05,
+                  }}
+                  refreshControl={
+                    <RefreshControl
+                      refreshing={refreshing}
+                      onRefresh={onRefresh}
+                    />
+                  }
+                />
+              </View>
+            </View>
+          </View>
           <BottomSheetModal
             ref={bottomSheetModalRef}
             index={1}
@@ -136,9 +166,10 @@ const ActivityScreen = ({ navigation }) => {
               />
             )}
             backgroundStyle={{ borderRadius: 30 }}
+            $modal={true}
           >
             <View className="items-center">
-              <Text>Awesome 🎉</Text>
+              <Text>{selectedActivity?.tiketId}</Text>
             </View>
           </BottomSheetModal>
         </BottomSheetModalProvider>
--- src/store/activitySlice.js
+++ src/store/activitySlice.js
@@ -3,10 +3,18 @@ import activity from "../data/activity";
 
 const initialState = {
   activityItems: activity,
+  selectedActivity: null,
 };
 
 export const activitySlice = createSlice({
   name: "activityItems",
   initialState,
-  reducers: {},
+  reducers: {
+    setSelectedActivity: (state, action) => {
+      const activityId = action.payload;
+      state.selectedActivity = state.activityItems.find(
+        (p) => p.id === activityId
+      );
+    },
+  },
 });
